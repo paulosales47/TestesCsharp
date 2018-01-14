5bdc3653b9fd21b0298a29c8c3684dd9dec30369
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2018-01-14 19:59:06 -0200

    Teste de classe ano bissexto

---
+++ TestesCsharp/AnoBissexto.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestesCsharp
+{
+    public class AnoBissexto
+    {
+        public bool EhBissexto(int ano)
+        {
+
+            if (ano % 400 == 0) return true;
+
+            else if (ano % 100 == 0) return false;
+
+            else if (ano % 4 == 0) return true;
+
+            else return false;
+
+        }
+    }
+}
---
+++ TestesCsharp/AnoBissextoTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace TestesCsharp
+{
+    [TestFixture]
+    public class AnoBissextoTest
+    {
+        [Test]
+        public void VerificaAnoBissexto()
+        {
+            AnoBissexto anoBissexto = new AnoBissexto();
+
+            for (int i = 0; i < 4000; i += 4)
+            {
+                if ((i % 100 == 0) && (i % 400 != 0))
+                    Assert.AreEqual(false, anoBissexto.EhBissexto(i));
+                else
+                    Assert.AreEqual(true, anoBissexto.EhBissexto(i));
+            }
+
+            for (int i = 0; i < 100000; i += 100)
+            {
+                if(i % 400 != 0)
+                    Assert.AreEqual(false, anoBissexto.EhBissexto(i));
+            }
+
+            for (int i = 0; i < 400000; i += 400)
+            {
+                 Assert.AreEqual(true, anoBissexto.EhBissexto(i));
+            }
+            
+        }
+
+
+    }
+}
